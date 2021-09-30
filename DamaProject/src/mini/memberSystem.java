56f1f0254352f3f5565df5f13efa4f1951ec986b
smhrd <smhrd@211.107.188.221>
2021-09-30 13:45:29 +0900

    가영 수정 (메인 절 병원관련 수정, 욕구성공실패 출력 , 실패이미지메소드 추가  )

--- DamaProject/src/mini/memberSystem.java
+++ DamaProject/src/mini/memberSystem.java
@@ -6,298 +6,158 @@ import java.util.Scanner;
 public class memberSystem {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	      Scanner sc = new Scanner(System.in);
 
-		System.out.println("브랜치 지움!");
-		int choice = 0; // 키우기 상태선택
-		System.out.println("다마고치 키우기");
-		memberDao dao = new memberDao();
-		img1 img = new img1();
-		while (true) {
-			System.out.print("1. 회원가입 2. 로그인 3. 게임 종료 >> ");
-			int main = sc.nextInt();
-			if (main == 1) {
-				System.out.println("--회원가입--");
-				System.out.print("ID입력 : ");
-				String id = sc.next();
-				System.out.print("PW입력 : ");
-				String pw = sc.next();
+	      System.out.println("다마고치 키우기");
+	      memberDao dao = new memberDao();
 
-				memberVO vo = new memberVO(id, pw);
+	      while (true) {
+	         System.out.print("1. 회원가입 2. 로그인 3. 게임 종료 >> ");
+	         int main = sc.nextInt();
+	         if (main == 1) {
+	            System.out.println("--회원가입--");
+	            System.out.print("ID입력 : ");
+	            String id = sc.next();
+	            System.out.print("PW입력 : ");
+	            String pw = sc.next();
 
-				int cnt = dao.join(vo);
+	            memberVO vo = new memberVO(id, pw);
 
-				if (cnt > 0)
-					System.out.println("회원가입 성공!");
-				else
-					System.out.println("회원가입 실패...");
-			} else if (main == 2) {
-<<<<<<< HEAD
-				System.out.println("=== 로그인 ===");
-=======
-				System.out.println("--로그인--");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-6/JavaChip.git
-				System.out.print("ID입력 : ");
-				String id = sc.next();
-				System.out.print("PW입력 : ");
-				String pw = sc.next();
+	            int cnt = dao.join(vo);
 
-				memberVO vo = new memberVO(id, pw);
+	            if (cnt > 0)
+	               System.out.println("회원가입 성공!");
+	            else
+	               System.out.println("회원가입 실패...");
+	         } else if (main == 2) {
+	            System.out.println("=== 로그인 ===");
+	            System.out.print("ID입력 : ");
+	            String id = sc.next();
+	            System.out.print("PW입력 : ");
+	            String pw = sc.next();
 
-				memberVO info = dao.login(vo);
+	            memberVO vo = new memberVO(id, pw);
 
-				if (info != null) {
-					System.out.println("로그인 성공!");
-					System.out.println(info.getId() + "님 환영합니다!");
-<<<<<<< HEAD
+	            memberVO info = dao.login(vo);
 
-					while (true) {
-						System.out.print("1.다마고치 등록 2.다마고치 키우기 3.다마고치 정보확인 4.랭킹 확인 5.종료 >> ");
-						int choose = sc.nextInt();
-						int choice = 0; // 키우기 상태선택
-						if (choose == 1) {
-							System.out.println("=== 다마고치 등록 ===");
-							System.out.print("NICK입력 : ");
-							String nick = sc.next();
+	            if (info != null) {
+	               System.out.println("로그인 성공!");
+	               System.out.println(info.getId() + "님 환영합니다!");
 
-							memberVO vo1 = new memberVO(nick, id, pw);// 지역변수라 위에도씀
+	               while (true) {
+	                  System.out.print("1.다마고치 등록 2.다마고치 키우기 3.다마고치 정보확인 4.랭킹 확인 5.종료 >> ");
+	                  int choose = sc.nextInt();
+	                  int choice = 0; // 키우기 상태선택
+	                  if (choose == 1) {
+	                     System.out.println("=== 다마고치 등록 ===");
+	                     System.out.print("NICK입력 : ");
+	                     String nick = sc.next();
 
-							int cnt = dao.make(vo1);
+	                     memberVO vo1 = new memberVO(nick, id, pw);// 지역변수라 위에도씀
 
-							if (cnt > 0)
-								System.out.println(nick + "생성!");
-							else
-								System.out.println("생성 실패...");
-						} else if (choose == 2) { // 키우기
-							memberVO exp = new memberVO(id);
+	                     int cnt = dao.make(vo1);
 
-							while (true) { // 랜덤 출력
-								String nickname = dao.nickPrint(vo);
-								String[] desire = { "는 배고파!!", "는 졸려!!", "는 피곤해!!", "는 공부해야지.. ", "는 아파.. " };
-								int count = 0;
-								while (true) {
-									count = (int) (Math.random() * 5); // 1-5개?
-									System.out.println(nickname + desire[count]);
-									break;
-								}
+	                     if (cnt > 0)
+	                        System.out.println(nick + "생성!");
+	                     else
+	                        System.out.println("생성 실패...");
+	                  } else if (choose == 2) { // 키우기
+	                     memberVO exp = new memberVO(id);
 
-								System.out.print("1.밥먹기 2.잠자기 3.휴식하기 4.공부하기 5.병원가기 6.종료 >> ");
-								String num = sc.next();
+	                     while (true) { // 랜덤 출력
+	                        String nickname = dao.nickPrint(vo);
+	                        String[] desire = { "는 배고파!!", "는 졸려!!", "는 피곤해!!", "는 공부해야지.. ", "는 아파.. " };
+	                        int count = 0;
+	                        while (true) {
+	                           count = (int) (Math.random() * 5); // 1-5개?
+	                           System.out.println(nickname + desire[count]);
+	                           break;
+	                        }
 
-								if (num.equals("1")) { // 밥먹기
-									if (count == 0) {
-										img.food();
-										dao.expplus(exp);
-									} else {
-										img.fail(); // 욕구 불일치시
-										dao.expminus(exp);
-									}
+	                        System.out.print("1.밥먹기 2.잠자기 3.휴식하기 4.공부하기 5.병원 6.종료 >> ");
+	                        String num = sc.next();
 
-								} else if (num.equals("2")) { // 잠자기
-									if (count == 1) {
-										img.sleep();
-										dao.expplus(exp);
-									} else {
-										img.fail();
-										dao.expminus(exp);
-									}
-								} else if (num.equals("3")) { // 휴식하기
-									if (count == 2) {
-										img.rest();
-										dao.expplus(exp);
-									} else {
-										img.fail();
-										dao.expminus(exp);
-									}
-								} else if (num.equals("4")) { // 공부하기
-									if (count == 3) {
-										img.study();
-										dao.expplus(exp);
-									} else {
-										img.fail();
-										dao.expminus(exp);
-									}
-								} else if (num.equals("5")) { // 병원가기
-									if (count == 4) {
-										img.hospital();
-										dao.expplus(exp);
-									} else {
-										img.fail();
-										dao.expminus(exp);
-									}
-								} else if (num.equals("6")) {
-									System.out.println("그만하기");
-									break;
-								} else {
-									System.out.println("잘못입력하셨습니다.");
-								}
-								if (Integer.parseInt(dao.exp(exp)) >= 100) {
-									dao.lvup(exp);
-									System.out.println("레벨 업");
-									dao.exp0(exp);
-								}
-							}
-						} else if (choose == 3) { // 다마고치 정보확인
-							System.out.println("===정보확인===");
-							img.nowStatus();
-							dao.checkStatus(vo);
+	                        if (num.equals("1")) { // 밥먹기
+	                           if (count == 0) {
+	                              img.food();
+	                              dao.expplus(exp);
+	                           } else {
+	                              img.fail(); // 욕구 불일치시
+	                              dao.expminus(exp);
+	                           }
 
-						} else if (choose == 4) { // 다마고치 랭킹확인
-							System.out.println("===랭킹===");
-							ArrayList<memberVO> list = dao.Rank();
-							for (int i = 0; i < list.size(); i++) {
-								System.out.println((i + 1) + "등 " + list.get(i));
-							}
-						} else if (choose == 5) { // 프로그램종료
-							System.out.println("종료");
-							break;
-							// 종료
-						} else
-							System.out.println("잘못입력하셨습니다.");
-					}
-=======
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-6/JavaChip.git
-				}
+	                        } else if (num.equals("2")) { // 잠자기
+	                           if (count == 1) {
+	                              img.sleep();
+	                              dao.expplus(exp);
+	                           } else {
+	                              img.fail();
+	                              dao.expminus(exp);
+	                           }
+	                        } else if (num.equals("3")) { // 휴식하기
+	                           if (count == 2) {
+	                              img.rest();
+	                              dao.expplus(exp);
+	                           } else {
+	                              img.fail();
+	                              dao.expminus(exp);
+	                           }
+	                        } else if (num.equals("4")) { // 공부하기
+	                           if (count == 3) {
+	                              img.study();
+	                              dao.expplus(exp);
+	                           } else {
+	                              img.fail();
+	                              dao.expminus(exp);
+	                           }
+	                        } else if (num.equals("5")) { // 병원가기
+	                           if (count == 4) {
+	                              img.hospital();
+	                              dao.expplus(exp);
+	                           } else {
+	                              img.fail();
+	                              dao.expminus(exp);
+	                           }
+	                        } else if (num.equals("6"))
+	                           break;
+	                        else
+	                           System.out.println("잘못입력하셨습니다.");
 
-<<<<<<< HEAD
-				else
-					System.out.println("로그인 실패...");
+	                     }
+	                  }
 
-			} else if (main == 3) {
-=======
-				while (true) {
-					System.out.print("1.다마고치 등록 2.다마고치 키우기 3.다마고치 정보확인 4.랭킹 확인 5.종료 >> ");
-					int choose = sc.nextInt();
-					if (choose == 1) {
-						System.out.println("===다마고치 등록===");
-						System.out.print("NICK입력 : ");
-						String nick = sc.next();
-						memberVO vo1 = new memberVO(nick, id, pw);// 지역변수라 위에도씀
-						int cnt = dao.make(vo1);
-						if (cnt > 0) {
-							System.out.println(nick + "생성!");
-							img.birth();
-						} else
-							System.out.println("생성 실패...");
-					} else if (choose == 2) {
-						while (true) {
-							// 랜덤 출력
-							String[] desire = { "배고파", "졸려", "피곤해", "공부해야지" };
-							int count = 0;
-							while (true) {
-								count = (int) (Math.random() * 4); // 1-5개?
-								System.out.println(desire[count]);
-								break;
-							}
-							memberVO exp = new memberVO(id);
+	                  else if (choose == 3) { // 다마고치 정보확인
+	                     System.out.println("===정보확인===");
+	                     img.nowStatus();
+	                     dao.checkStatus(vo);
 
-							System.out.print("1.밥먹기 2.잠자기 3.휴식하기 4.공부하기 5.종료 >> ");
-							String num = sc.next();
+	                  } else if (choose == 4) { // 다마고치 랭킹확인
+	                     // 랭킹
+	                     System.out.println("===랭킹===");
+	                     ArrayList<memberVO> list = dao.Rank();
+	                     for (int i = 0; i < list.size(); i++) {
+	                        System.out.println(list.get(i));
+	                     }
+	                  } else if (choose == 5) { // 프로그램종료
+	                     System.out.println("종료");
+	                     break;
+	                     // 종료
+	                  } else
+	                     System.out.println("잘못입력하셨습니다.");
+	               }
+	            }
 
-							if (num.equals("1")) {
-								// 밥먹는 이미지
-								img.food();
-								if (count == 0)
-									dao.expplus(exp);
-								else
-									dao.expminus(exp);
+	            else
+	               System.out.println("로그인 실패...");
 
-							} else if (num.equals("2")) {
-								// 잠자는 이미지
-								img.sleep();
-								if (count == 1)
-									dao.expplus(exp);
-								else
-									dao.expminus(exp);
+	         } else if (main == 3) {
+	            System.out.println("종료합니다.");
+	            break;
+	         } else
+	            System.out.println("잘못입력하셨습니다.");
 
-							} else if (num.equals("3")) {
-								// 휴식 이미지
-								img.rest();
-								if (count == 2)
-									dao.expplus(exp);
-								else
-									dao.expminus(exp);
+	      }
 
-							} else if (num.equals("4")) {
-								// 공부 이미지
-								img.study();
-								if (count == 3)
-									dao.expplus(exp);
-								else
-									dao.expminus(exp);
-
-							} else if (num.equals("5")) {
-							} else if (choose == 2) { // 현재상태 + 키우기 상태창
-								while (true) {
-									// 별명 + 상태 랜덤 출력
-									System.out.print("현재 ");
-									dao.nickPrint(vo);
-									System.out.print("의 상태 >> ");
-									dao.randomDesire();
-									System.out.print("1.밥먹기 2.잠자기 3.휴식하기 4.공부하기 5.병원가기 6.이전으로 >> ");
-									String input = sc.next(); // --> num.equals("1") 코드
-									// int c = sc.nextInt();
-									if (input.equals("1")) { // 밥먹기
-										Image.food();
-										// if(dao.randomDesire()==dao.exp())
-									} else if (input.equals("2")) { // 잠자기
-										Image.sleep();
-									} else if (input.equals("3")) { // 휴식하기
-										Image.rest();
-									} else if (input.equals("4")) { // 공부하기
-										Image.study();
-
-									} else if (input.equals("5")) { // 병원가기
-										Image.hospital();
-
-									} else if (num.equals("6"))
-										break;
-									else
-										System.out.println("잘못입력하셨습니다.");
-									if (Integer.parseInt(dao.exp(exp)) >= 100) {
-										dao.lvup(exp);
-										dao.checkStatus(vo);
-										dao.exp0(exp);
-									}
-								}
-							} else if (choose == 3) {
-								System.out.println("===정보확인===");
-								Image.nowStatus();
-								dao.checkStatus(vo);
-							}
-
-							else if (choose == 4) { // 다마고치 랭킹확인
-								// 랭킹
-								System.out.println("===랭킹===");
-								ArrayList<memberVO> list = dao.Rank();
-								for (int i = 0; i < list.size(); i++) {
-									System.out.println((i + 1) + "등 " + list.get(i));
-								}
-							} else if (choose == 5) { // 프로그램종료
-								System.out.println("종료");
-								break;
-							} else
-								System.out.println("잘못입력하셨습니다.");
-						}
-					} else
-						System.out.println("로그인 실패...");
-
-				}
-
-			} else if (main == 3)
-
-			{
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-6/JavaChip.git
-				System.out.println("종료합니다.");
-				break;
-			} else
-				System.out.println("잘못입력하셨습니다.");
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-6/JavaChip.git
-		}
-
-	}
+	   }
 
 }
